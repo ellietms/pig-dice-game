91956e648137eeaec306b887e6d57264592958d4
ellie <ellie.tms1371@gmail.com>
2020-05-24 22:53:55 +0100

    Update piggame.js

--- piggame.js
+++ piggame.js
@@ -28,6 +28,9 @@ document.querySelector(".btn-roll").addEventListener("click",function(){
         roundScore = 0;
         document.getElementById("current-0").textContent = "0";
         document.getElementById("current-1").textContent = "0";
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        document.querySelector(".dice").style.display = "none";
     }
 
 })
