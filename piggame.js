1246ea45ebc2d9671d6daca38ee13e5769788fad
ellie <ellie.tms1371@gmail.com>
2020-05-24 22:40:19 +0100

    set defualt to zero

--- piggame.js
+++ piggame.js
@@ -6,12 +6,17 @@ activePlayer = 0;
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 // let x = document.querySelector("#score-0").textContent;
 document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
+
 document.querySelector(".btn-roll").addEventListener("click",function(){
 //   1.random  number
   let   dice =  Math.floor(Math.random() * 6) + 1;
 // 2.Display the result
     let diceDOM = document.querySelector(".dice");
     diceDOM.style.display = "block";
-    diceDOM.src = "dice-" + dice + "png";
+    diceDOM.src = "dice-" + dice + ".png";
 
 })
