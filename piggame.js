97e05965e6637aa0af9626dbd3fdd672b647174b
ellie <ellie.tms1371@gmail.com>
2020-05-24 22:20:51 +0100

    Update piggame.js

--- piggame.js
+++ piggame.js
@@ -1,7 +1,9 @@
-let scores, roundScore, activePlayer , dice;
+let scores,roundScore,activePlayer,dice;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 document.querySelector("#current-" + activePlayer).textContent = dice;
+let x = document.querySelector("#score-0").textContent;
+document.querySelector(".dice").style.display = "none";
